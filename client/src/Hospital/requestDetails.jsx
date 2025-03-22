63081a911f32d8a39eb82270ffb4d0b3fe792fb0
keerthi-22-2005 <23505a4403@pvpsit.ac.in>
2025-03-22 21:22:55 +0530

    Update requestDetails.jsx

--- client/src/Hospital/requestDetails.jsx
+++ client/src/Hospital/requestDetails.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/authContext";
 
 const RequestDetails = () => {
   const { user } = useContext(AuthContext);
