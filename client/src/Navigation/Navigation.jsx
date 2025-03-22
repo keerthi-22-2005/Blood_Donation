7e47cfb6e73ae8687060f7dec4268c1181d02128
keerthi-22-2005 <23505a4403@pvpsit.ac.in>
2025-03-22 21:22:00 +0530

    Update Navigation.jsx

--- client/src/Navigation/Navigation.jsx
+++ client/src/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/authContext";
 
 export default function Navigation() {
   const { user, logout } = useContext(AuthContext); // Access user state and logout function
