8620e5ade2d45c13ca222582142a362fdeedcdc2
keerthi-22-2005 <23505a4403@pvpsit.ac.in>
2025-03-22 11:57:14 +0530

    Update db.js

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 const connectDB= async ()=>{
     try{
-        await mongoose.connect(process.env.mongoDB_Url)
+        await mongoose.connect("mongodb+srv://keerthi:keerthi@mongodb-1.wo3j6.mongodb.net/blooddonation?retryWrites=true&w=majority&appName=MongoDB-1")
             .then(()=>{
                 console.log("Mongodb connected successfully")
             })
