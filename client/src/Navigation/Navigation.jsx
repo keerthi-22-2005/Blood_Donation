bccc856a9c9fa4dc303ea79cb5d68c46e7341c96
keerthi-22-2005 <23505a4403@pvpsit.ac.in>
2025-05-07 23:06:44 +0530

    Update Navigation.jsx

--- client/src/Navigation/Navigation.jsx
+++ client/src/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../context/authContext";
+import { AuthContext } from "../Context/authContext";
 
 
 export default function Navigation() {
